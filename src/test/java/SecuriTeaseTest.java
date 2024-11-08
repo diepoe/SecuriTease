fb596ee747d82a76cfe01026cefb900f36a3e7e7
Dietrich Poensgen <poedietrich@outlook.de>
2024-11-08 14:52:45 +0100

    add source file for unit tests

---
+++ src/test/java/SecuriTeaseTest.java
@@ -0,0 +1,13 @@
+import com.cthiebaud.passwordvalidator.ValidationResult;
+import com.diepoe.securitease.SecuriTease;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecuriTeaseTest {
+    @Test
+    public void test() {
+        SecuriTease securiTease = new SecuriTease();
+        assertEquals(securiTease.validate("password").isValid(), true);
+    }
+}
