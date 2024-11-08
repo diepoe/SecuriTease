9a3bc080d3d57f9ac054c1f4b352503e91313673
Dietrich Poensgen <poedietrich@outlook.de>
2024-11-08 14:52:28 +0100

    code comments

--- src/main/java/com/diepoe/securitease/SecuriTease.java
+++ src/main/java/com/diepoe/securitease/SecuriTease.java
@@ -3,8 +3,21 @@ package com.diepoe.securitease;
 import com.cthiebaud.passwordvalidator.PasswordValidator;
 import com.cthiebaud.passwordvalidator.ValidationResult;
 
+/**
+ * This class bundles the application logic while implementing the
+ * PasswordValidator interface
+ * 
+ * @implNote PasswordValidator prerequisite for the project design
+ */
 public class SecuriTease implements PasswordValidator {
+    /**
+     * main validation method
+     * 
+     * @param potentialPassord the password to validate
+     */
     public ValidationResult validate(String potentialPassord) {
         return new ValidationResult(true, "Passwordo e perfecto!");
     }
+
+    // TODO create various types of checking methods for different yet-to-specify rules
 }
