039d4effc69adb69f38c99afd62363432c9b0126
Dietrich Poensgen <poedietrich@outlook.de>
2024-11-12 21:29:51 +0100

    include all Rules from the list in the validation process

--- src/main/java/com/diepoe/securitease/SecuriTease.java
+++ src/main/java/com/diepoe/securitease/SecuriTease.java
@@ -17,8 +17,8 @@ public class SecuriTease implements PasswordValidator {
     private List<Rule> rules;
 
     public SecuriTease() {
-        // TODO: implement randomized setting of the rules
-        rules = new ArrayList<>();
+        // TODO: implement randomized setting of the ruleset
+        rules = new ArrayList<>(10);
 
         rules.add(new Rule(this::checkLength, new String[] { "Password must be at least 8 characters long" }, 8));
         rules.add(new Rule(this::checkRomanLiteralSum,
@@ -31,12 +31,17 @@ public class SecuriTease implements PasswordValidator {
      * @param potentialPassord the password to validate
      */
     public ValidationResult validate(String potentialPassord) {
-        // TODO implement usage of the rules list
+        boolean valid = true;
+        String message = "[Slow clap] Ohhh, congratulations, you finally managed to enter a valid password. Want a cookie for doing the absolute bare minimum? 🙄";
 
-        Rule rule = rules.get(1);
-        CheckingFunction checker = rule.getCheckingFunction();
-        boolean valid = checker.check(potentialPassord, rule.getThreshold());
-        String message = rule.getFeedbackMessage()[0];
+        for (Rule rule : rules) {
+            boolean ruleValid = rule.getCheckingFunction().check(potentialPassord, rule.getThreshold());
+            if (!ruleValid) {
+                valid = false;
+                message = rule.getFeedbackMessage()[0];
+                break;
+            }
+        }
 
         return new ValidationResult(valid, message);
     }
@@ -47,14 +52,24 @@ public class SecuriTease implements PasswordValidator {
     /**
      * Checks if a given password meets a certain length criteria
      * 
-     * @param password String - the password to be checked
-     * @param requiredLength int - the minimal length of the password string
-     * @return boolean - String.length >= int
+     * @param password       the string to be checked
+     * @param requiredLength the minimal length (int) of the password string
+     * @return true if the string length is bigger or equal than the required length
      */
     private boolean checkLength(String password, int requiredLength) {
         return password.length() >= requiredLength;
     }
 
+    /**
+     * Checks if the sum of the Roman numeral literals in the given password equals
+     * the required sum (threshold).
+     *
+     * @param password    the string containing Roman numeral literals to be checked
+     * @param requiredSum the sum that the Roman numeral literals in the password
+     *                    should equal
+     * @return true if the sum of the Roman numeral literals equals the required
+     *         sum, false otherwise
+     */
     private boolean checkRomanLiteralSum(String password, int requiredSum) {
         Map<Character, Integer> romanValues = Map.of(
                 'I', 1,
