fb2b9218e8462399902ae23954e52fe5727b16c3
diepoe <poedietrich@outlook.de>
2024-12-01 21:57:26 +0100

    throw exception if password is null

--- src/main/java/com/diepoe/securitease/SecuriTease.java
+++ src/main/java/com/diepoe/securitease/SecuriTease.java
@@ -102,7 +102,11 @@ public class SecuriTease implements PasswordValidator {
      * @param potentialPassword the password to validate
      */
     @Override
-    public ValidationResult validate(String potentialPassword) {
+    public ValidationResult validate(String potentialPassword) throws IllegalArgumentException {
+        if (potentialPassword == null) {
+            throw new IllegalArgumentException("Password cannot be null");
+        }
+
         boolean valid = true;
         String message = "[Slow clap] Ohhh, congratulations, you finally managed to enter a valid password. Want a cookie for doing the absolute bare minimum? 🙄";
 
